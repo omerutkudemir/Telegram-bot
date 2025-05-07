cf8c6f0e46859cb06eee6513f28ea9508763779c
Enskc05 <kenes7667@gmail.com>
2025-05-08 02:08:00 +0300

    güncellendi build

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-set -e  # Hata olursa dur
-# Sistem güncellemeleri ve bağımlılıklar
+set -e
 apt-get update
-apt-get install -y wget unzip libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
-# Chrome'u yükle
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-# Chrome yolunu doğrula
-if [ -f "/usr/bin/google-chrome" ]; then
-    echo "Chrome başarıyla yüklendi"
-else
-    echo "HATA: Chrome yüklenemedi"
-    exit 1
-fi
-# Python bağımlılıklarını yükle
+apt-get install -y google-chrome-stable
 pip install -r requirements.txt
